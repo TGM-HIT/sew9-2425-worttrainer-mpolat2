218387a9972eb146ca260f80060740a8808f3013
mpolat2 <mpolat@student.tgm.ac.at>
2024-11-19 09:55:47 +0100

    JSON Klasse und Speicherstrategie Interface erstellen

---
+++ src/main/java/at.ac.tgm.mpolat.Model/Speicherstrategie.java
@@ -0,0 +1,19 @@
+package at.ac.tgm.mpolat.Model;
+
+/**
+ * Speicherstrategie-Interface für das Speichern und Laden von RechtschreibTrainer
+ * @author Polat Enes
+ * @version 18.9.2024
+ */
+interface Speicherstrategie {
+    /**
+     * Speichern von einer Rechtschreibtrainer Liste
+     * @param trainer Der Trainer
+     */
+    void save(Rechtschreibtrainer trainer);
+    /**
+     * Laden eines Rechtschreibtrainers basiert auf einer Liste
+     * @return den Trainer
+     */
+    Rechtschreibtrainer load();
+}
