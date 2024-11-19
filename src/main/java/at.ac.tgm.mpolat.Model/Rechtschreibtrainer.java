1a551cba526dffd3fd778b4d4cc07c8ad74f4561
mpolat2 <mpolat@student.tgm.ac.at>
2024-11-19 08:43:41 +0100

    Rechtschreibtrainer und WorltBIldPaar Klasse fertigen

--- src/main/java/at.ac.tgm.mpolat.Model/Rechtschreibtrainer.java
+++ src/main/java/at.ac.tgm.mpolat.Model/Rechtschreibtrainer.java
@@ -1,72 +1,74 @@
 package at.ac.tgm.mpolat.Model;
 
 import java.util.ArrayList;
-
+import java.util.List;
+import java.util.Random;
+/**
+ * Übernimmt die Logik des Wort checkens
+ * @author Polat Enes
+ * @version 18.9.2024
+ */
 public class Rechtschreibtrainer {
-
+    private ArrayList<WortBildPaar> wortBildPaare;
+    private WortBildPaar ausgewaehltesPaar;
+    private int versucheGesamt;
+    private int versucheRichtig;
+    private int versucheFalsch;
     /**
-     * @author Muhammed Enes Polat
-     * @version 18.09.2024
-     *
+     * Konstruktor für den Trainer, es werden eigene Paare erstellt
      */
-
-    private ArrayList<WoldPicturePair> pairList;
-    private int selectePairIndex = 0;
-    private int rightCounter = 0;
-    private int falseCounter = 0;
-    private int answerCounter = 0;
-
-    public Rechtschreibtrainer() {
-
+    public Rechtschreibtrainer(){
+        wortBildPaare = new ArrayList<>();
+        wortBildPaare.add(new WortBildPaar("Apfel", "https://res.cloudinary.com/saas-ag/image/upload/w_1200,h_630,b_white,c_pad,q_auto,f_auto/v1664972437/mpreis/products/ac8c0f84-00fa-4ae1-8f75-d32e6b9d5973.jpg"));
+        wortBildPaare.add(new WortBildPaar("Banane", "https://t4.ftcdn.net/jpg/00/64/75/97/360_F_64759733_oLj6dBUvvHeezmTluE7bOW7pW73uWjjA.jpg"));
+        wortBildPaare.add(new WortBildPaar("Birne", "https://www.kochschule.de/sites/default/files/images/kochwissen/470/birne.jpg"));
+        wortBildPaare.add(new WortBildPaar("Orange", "https://www.kochschule.de/sites/default/files/images/kochwissen/432/orange.jpg"));
+        wortBildPaare.add(new WortBildPaar("Ananas", "https://know-how.mnstry.com/wp-content/uploads/2021/01/Ananas.jpg"));
+        versucheGesamt = 0;
+        versucheRichtig = 0;
+        versucheFalsch = 0;
     }
-
-    public Rechtschreibtrainer(ArrayList<WoldPicturePair> pairList) {
-        this.pairList = pairList;
+    /**
+     * Wählt ein zufällige Paar von der Liste aus und speichert es
+     */
+    public void waehleZufaelligesPaar(){
+        if (!wortBildPaare.isEmpty()) {
+            Random rand = new Random();
+            int randomIndex = rand.nextInt(wortBildPaare.size()); // generates a random index
+            ausgewaehltesPaar = wortBildPaare.get(randomIndex); // sets the randomly selected pair
+        } else {
+            throw new NullPointerException("Leere Liste!");
+        }
     }
-
-    public int selectPair(int index){
-
+    /**
+     * Das eingegebene Wort wird mit der Lösung verglichen. Bei falscheingabe bleibt das Bild
+     * ansonsten geht es zum nächsten.
+     * @param wort Geratene Wort
+     * @return ob es richtig erraten wurde
+     */
+    public boolean rateWort(String wort){
+        this.versucheGesamt++;
+        if(wort.equals(this.ausgewaehltesPaar.getWort())){
+            this.versucheRichtig++;
+            this.waehleZufaelligesPaar();
+            return true;
+        } else {
+            this.versucheFalsch++;
+            return false;
+        }
     }
-
-    public int selcetRandomPair(){
-
+    /**
+     * Statistik zu dem aktuellen Punktestand
+     * @return die Statistik
+     */
+    public String statistik(){
+        return "Stats - Gesamt: " + this.versucheGesamt + ", Richtig: " + this.versucheRichtig + ", Falsch: " + this.versucheFalsch;
     }
-
-    public boolean checkAnswer(String answer){
-
+    /**
+     * Das aktuelle Paar wird zurückgegeben
+     * @return aktuelle Paar
+     */
+    public WortBildPaar getAusgewaehltesPaar() {
+        return ausgewaehltesPaar;
     }
-
-    public void setPairList(ArrayList<WoldPicturePair> pairList) {
-        this.pairList = pairList;
-    }
-
-    public void setSelectePairIndex(int selectePairIndex) {
-        this.selectePairIndex = selectePairIndex;
-    }
-
-    public void setRightCounter(int rightCounter) {
-        this.rightCounter = rightCounter;
-    }
-
-    public ArrayList<WoldPicturePair> getPairList() {
-        return pairList;
-    }
-
-    public WoldPicturePair getSelectedPair() {
-        return pairList.get(selectePairIndex);
-    }
-
-    public int getRightCounter() {
-        return rightCounter;
-    }
-
-    public int getFalseCounter() {
-        return falseCounter;
-    }
-
-    public int getAnswerCounter() {
-        return answerCounter;
-    }
-
 }
-
